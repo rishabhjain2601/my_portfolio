ebc7e9daf4c82ffaa0437e10dff8e41354be07f2
rishabhjain2601 <rishabhjain2dtu@gmail.com>
2024-03-29 22:45:58 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,428 @@
+function files(index) {
+  var data = `
+     ../Images/male0001.png
+     ../Images/male0002.png
+     ../Images/male0003.png
+     ../Images/male0004.png
+     ../Images/male0005.png
+     ../Images/male0006.png
+     ../Images/male0007.png
+     ../Images/male0008.png
+     ../Images/male0009.png
+     ../Images/male0010.png
+     ../Images/male0011.png
+     ../Images/male0012.png
+     ../Images/male0013.png
+     ../Images/male0014.png
+     ../Images/male0015.png
+     ../Images/male0016.png
+     ../Images/male0017.png
+     ../Images/male0018.png
+     ../Images/male0019.png
+     ../Images/male0020.png
+     ../Images/male0021.png
+     ../Images/male0022.png
+     ../Images/male0023.png
+     ../Images/male0024.png
+     ../Images/male0025.png
+     ../Images/male0026.png
+     ../Images/male0027.png
+     ../Images/male0028.png
+     ../Images/male0029.png
+     ../Images/male0030.png
+     ../Images/male0031.png
+     ../Images/male0032.png
+     ../Images/male0033.png
+     ../Images/male0034.png
+     ../Images/male0035.png
+     ../Images/male0036.png
+     ../Images/male0037.png
+     ../Images/male0038.png
+     ../Images/male0039.png
+     ../Images/male0040.png
+     ../Images/male0041.png
+     ../Images/male0042.png
+     ../Images/male0043.png
+     ../Images/male0044.png
+     ../Images/male0045.png
+     ../Images/male0046.png
+     ../Images/male0047.png
+     ../Images/male0048.png
+     ../Images/male0049.png
+     ../Images/male0050.png
+     ../Images/male0051.png
+     ../Images/male0052.png
+     ../Images/male0053.png
+     ../Images/male0054.png
+     ../Images/male0055.png
+     ../Images/male0056.png
+     ../Images/male0057.png
+     ../Images/male0058.png
+     ../Images/male0059.png
+     ../Images/male0060.png
+     ../Images/male0061.png
+     ../Images/male0062.png
+     ../Images/male0063.png
+     ../Images/male0064.png
+     ../Images/male0065.png
+     ../Images/male0066.png
+     ../Images/male0067.png
+     ../Images/male0068.png
+     ../Images/male0069.png
+     ../Images/male0070.png
+     ../Images/male0071.png
+     ../Images/male0072.png
+     ../Images/male0073.png
+     ../Images/male0074.png
+     ../Images/male0075.png
+     ../Images/male0076.png
+     ../Images/male0077.png
+     ../Images/male0078.png
+     ../Images/male0079.png
+     ../Images/male0080.png
+     ../Images/male0081.png
+     ../Images/male0082.png
+     ../Images/male0083.png
+     ../Images/male0084.png
+     ../Images/male0085.png
+     ../Images/male0086.png
+     ../Images/male0087.png
+     ../Images/male0088.png
+     ../Images/male0089.png
+     ../Images/male0090.png
+     ../Images/male0091.png
+     ../Images/male0092.png
+     ../Images/male0093.png
+     ../Images/male0094.png
+     ../Images/male0095.png
+     ../Images/male0096.png
+     ../Images/male0097.png
+     ../Images/male0098.png
+     ../Images/male0099.png
+     ../Images/male0100.png
+     ../Images/male0101.png
+     ../Images/male0102.png
+     ../Images/male0103.png
+     ../Images/male0104.png
+     ../Images/male0105.png
+     ../Images/male0106.png
+     ../Images/male0107.png
+     ../Images/male0108.png
+     ../Images/male0109.png
+     ../Images/male0110.png
+     ../Images/male0111.png
+     ../Images/male0112.png
+     ../Images/male0113.png
+     ../Images/male0114.png
+     ../Images/male0115.png
+     ../Images/male0116.png
+     ../Images/male0117.png
+     ../Images/male0118.png
+     ../Images/male0119.png
+     ../Images/male0120.png
+     ../Images/male0121.png
+     ../Images/male0122.png
+     ../Images/male0123.png
+     ../Images/male0124.png
+     ../Images/male0125.png
+     ../Images/male0126.png
+     ../Images/male0127.png
+     ../Images/male0128.png
+     ../Images/male0129.png
+     ../Images/male0130.png
+     ../Images/male0131.png
+     ../Images/male0132.png
+     ../Images/male0133.png
+     ../Images/male0134.png
+     ../Images/male0135.png
+     ../Images/male0136.png
+     ../Images/male0137.png
+     ../Images/male0138.png
+     ../Images/male0139.png
+     ../Images/male0140.png
+     ../Images/male0141.png
+     ../Images/male0142.png
+     ../Images/male0143.png
+     ../Images/male0144.png
+     ../Images/male0145.png
+     ../Images/male0146.png
+     ../Images/male0147.png
+     ../Images/male0148.png
+     ../Images/male0149.png
+     ../Images/male0150.png
+     ../Images/male0151.png
+     ../Images/male0152.png
+     ../Images/male0153.png
+     ../Images/male0154.png
+     ../Images/male0155.png
+     ../Images/male0156.png
+     ../Images/male0157.png
+     ../Images/male0158.png
+     ../Images/male0159.png
+     ../Images/male0160.png
+     ../Images/male0161.png
+     ../Images/male0162.png
+     ../Images/male0163.png
+     ../Images/male0164.png
+     ../Images/male0165.png
+     ../Images/male0166.png
+     ../Images/male0167.png
+     ../Images/male0168.png
+     ../Images/male0169.png
+     ../Images/male0170.png
+     ../Images/male0171.png
+     ../Images/male0172.png
+     ../Images/male0173.png
+     ../Images/male0174.png
+     ../Images/male0175.png
+     ../Images/male0176.png
+     ../Images/male0177.png
+     ../Images/male0178.png
+     ../Images/male0179.png
+     ../Images/male0180.png
+     ../Images/male0181.png
+     ../Images/male0182.png
+     ../Images/male0183.png
+     ../Images/male0184.png
+     ../Images/male0185.png
+     ../Images/male0186.png
+     ../Images/male0187.png
+     ../Images/male0188.png
+     ../Images/male0189.png
+     ../Images/male0190.png
+     ../Images/male0191.png
+     ../Images/male0192.png
+     ../Images/male0193.png
+     ../Images/male0194.png
+     ../Images/male0195.png
+     ../Images/male0196.png
+     ../Images/male0197.png
+     ../Images/male0198.png
+     ../Images/male0199.png
+     ../Images/male0200.png
+     ../Images/male0201.png
+     ../Images/male0202.png
+     ../Images/male0203.png
+     ../Images/male0204.png
+     ../Images/male0205.png
+     ../Images/male0206.png
+     ../Images/male0207.png
+     ../Images/male0208.png
+     ../Images/male0209.png
+     ../Images/male0210.png
+     ../Images/male0211.png
+     ../Images/male0212.png
+     ../Images/male0213.png
+     ../Images/male0214.png
+     ../Images/male0215.png
+     ../Images/male0216.png
+     ../Images/male0217.png
+     ../Images/male0218.png
+     ../Images/male0219.png
+     ../Images/male0220.png
+     ../Images/male0221.png
+     ../Images/male0222.png
+     ../Images/male0223.png
+     ../Images/male0224.png
+     ../Images/male0225.png
+     ../Images/male0226.png
+     ../Images/male0227.png
+     ../Images/male0228.png
+     ../Images/male0229.png
+     ../Images/male0230.png
+     ../Images/male0231.png
+     ../Images/male0232.png
+     ../Images/male0233.png
+     ../Images/male0234.png
+     ../Images/male0235.png
+     ../Images/male0236.png
+     ../Images/male0237.png
+     ../Images/male0238.png
+     ../Images/male0239.png
+     ../Images/male0240.png
+     ../Images/male0241.png
+     ../Images/male0242.png
+     ../Images/male0243.png
+     ../Images/male0244.png
+     ../Images/male0245.png
+     ../Images/male0246.png
+     ../Images/male0247.png
+     ../Images/male0248.png
+     ../Images/male0249.png
+     ../Images/male0250.png
+     ../Images/male0251.png
+     ../Images/male0252.png
+     ../Images/male0253.png
+     ../Images/male0254.png
+     ../Images/male0255.png
+     ../Images/male0256.png
+     ../Images/male0257.png
+     ../Images/male0258.png
+     ../Images/male0259.png
+     ../Images/male0260.png
+     ../Images/male0261.png
+     ../Images/male0262.png
+     ../Images/male0263.png
+     ../Images/male0264.png
+     ../Images/male0265.png
+     ../Images/male0266.png
+     ../Images/male0267.png
+     ../Images/male0268.png
+     ../Images/male0269.png
+     ../Images/male0270.png
+     ../Images/male0271.png
+     ../Images/male0272.png
+     ../Images/male0273.png
+     ../Images/male0274.png
+     ../Images/male0275.png
+     ../Images/male0276.png
+     ../Images/male0277.png
+     ../Images/male0278.png
+     ../Images/male0279.png
+     ../Images/male0280.png
+     ../Images/male0281.png
+     ../Images/male0282.png
+     ../Images/male0283.png
+     ../Images/male0284.png
+     ../Images/male0285.png
+     ../Images/male0286.png
+     ../Images/male0287.png
+     ../Images/male0288.png
+     ../Images/male0289.png
+     ../Images/male0290.png
+     ../Images/male0291.png
+     ../Images/male0292.png
+     ../Images/male0293.png
+     ../Images/male0294.png
+     ../Images/male0295.png
+     ../Images/male0296.png
+     ../Images/male0297.png
+     ../Images/male0298.png
+     ../Images/male0299.png
+     ../Images/male0300.png
+ `;
+  return data.split("\n")[index];
+}
+
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true ,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locomotive();
+
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function centerCanvas() {
+  const canvasWidth = canvas.offsetWidth;
+  const windowWidth = window.innerWidth;
+  const leftOffset = (windowWidth - canvasWidth) / 2;
+  gsap.set(canvas, { left: leftOffset });
+}
+
+// Call centerCanvas initially and on window resize
+centerCanvas();
+window.addEventListener("resize", centerCanvas);
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  render();
+});
+
+
+const frameCount = 300;
+
+const images = [];
+const imageSeq = {
+  frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = files(i);
+  images.push(img);
+}
+
+gsap.to(imageSeq, {
+  x: 40,
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: `none`,
+  scrollTrigger: {
+    scrub: 0.15,
+    trigger: `canvas`,
+    start: `top`,
+    end: `400%`,
+    scroller: `#main`,
+  },
+  onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+  scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.max(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
+
+ScrollTrigger.create({
+  trigger: "canvas",
+  pin: true,
+  // markers:true,
+  scroller: `#main`,
+  start: `bottom bottom`,
+  end: `600% top`,
+});
